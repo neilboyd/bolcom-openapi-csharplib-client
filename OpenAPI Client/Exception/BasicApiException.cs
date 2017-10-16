7fc752ecaa00aaf7478e8ef5d7391c976c2180c7
Neil Boyd <neilboyd@gmail.com>
2017-10-16 15:33:28 +0200

    Add StatusCode and ToString() to BasicApiException

--- OpenAPI Client/Exception/BasicApiException.cs
+++ OpenAPI Client/Exception/BasicApiException.cs
@@ -1,15 +1,23 @@
-﻿
+﻿using System.Net;
+
 namespace Bol.OpenAPI.Exception
 {
     public class BasicApiException : System.ApplicationException
     {
+        public HttpStatusCode StatusCode { get; set; }
         public string Status { get; set; }
 
         public BasicApiException() { }
 
-        public BasicApiException(string status, string message) : base(message)
-        {            
-            this.Status = status;
-        }        
+        public BasicApiException(HttpStatusCode statusCode, string status, string message) : base(message)
+        {
+            StatusCode = statusCode;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return $"[{StatusCode} {Status} {Message}]";
+        }
     }
 }
